5c78f828c3a3cff85c1b2c30c7d77998819be95a
Victor Perez <vjperez@mix.wvu.edu>
2020-08-23 21:51:43 -0400

    Updates

---
+++ precache-manifest.effa0d72183fba2bf1e779f6d4aad50a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4296386dfd63bb8461f457f716c8f3ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56c68b26e457a2b2289a",
+    "url": "/static/css/main.7dd9550f.chunk.css"
+  },
+  {
+    "revision": "f7959214beb2ef13e4bb",
+    "url": "/static/js/2.3eeda292.chunk.js"
+  },
+  {
+    "revision": "fbdb25c6a30511ca0525a561a8e4c009",
+    "url": "/static/js/2.3eeda292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c68b26e457a2b2289a",
+    "url": "/static/js/main.124f5581.chunk.js"
+  },
+  {
+    "revision": "06e58afb766e7a7c6194",
+    "url": "/static/js/runtime-main.99f91223.js"
+  },
+  {
+    "revision": "f19b0d5cb8b4dc9986fc3bcc552de2c4",
+    "url": "/static/media/Jaw.f19b0d5c.svg"
+  },
+  {
+    "revision": "22f9c9048596b8f7787bfe1dca7b76f4",
+    "url": "/static/media/RegRot.22f9c904.svg"
+  },
+  {
+    "revision": "e6ce2aa7e3b5a44ffad6812e1d1ad067",
+    "url": "/static/media/fancyDivider.e6ce2aa7.svg"
+  },
+  {
+    "revision": "ff900880c525551e45ccb0c23c93b76f",
+    "url": "/static/media/le-murmure.ff900880.otf"
+  },
+  {
+    "revision": "efa06a74a5042aac844c73158539d841",
+    "url": "/static/media/rania.efa06a74.svg"
+  },
+  {
+    "revision": "13eae32ca8f55630b338b2083bd7b4e6",
+    "url": "/static/media/wireless.13eae32c.svg"
+  }
+]);
